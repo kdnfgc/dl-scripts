0138758db97ece90009b96833992c43dbd541a82
kdnfgc <75000353+kdnfgc@users.noreply.github.com>
2021-01-09 13:34:27 -0700

    Add files via upload

--- redhat.sh
+++ redhat.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo yum install kitty vim engrampa bspwm htop leafpad neofetch nitrogen polybar sxhkd volumeicon networkmanager network-manager-applet
+sudo yum install dmenu kitty vim engrampa caja bspwm htop leafpad neofetch nitrogen polybar sxhkd redshift firefox
