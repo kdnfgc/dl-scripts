1821f2d41af5a9f9a26936fed09e8ca3dc197864
kdnfgc <75000353+kdnfgc@users.noreply.github.com>
2021-01-11 14:40:12 -0700

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo dnf install dunst dmenu kitty vim engrampa caja bspwm htop leafpad neofetch nitrogen polybar sxhkd redshift min scrot
+sudo dnf install dunst dmenu kitty vim engrampa caja bspwm htop leafpad neofetch nitrogen polybar sxhkd redshift min scrot eom
