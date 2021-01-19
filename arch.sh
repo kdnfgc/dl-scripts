54a89b10259e8a1884b0e1821b166fb3532be751
kdnfgc <ren18124@gmail.com>
2021-01-19 16:16:36 -0700

    modified:   arch.sh

--- arch.sh
+++ arch.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-<<<<<<< HEAD
-sudo pacman -S dunst dmenu kitty vim engrampa caja bspwm htop leafpad neofetch nitrogen polybar sxhkd redshift min scrot viewnior mupdf p7zip
-=======
-sudo pacman -S dunst dmenu kitty vim engrampa caja bspwm htop leafpad neofetch nitrogen polybar sxhkd redshift min scrot eom
->>>>>>> ec49d163c42e2386317b64e01d63b5986f95bc38
+sudo pacman -S dunst dmenu kitty vim engrampa caja bspwm htop leafpad neofetch nitrogen polybar sxhkd redshift min scrot viewnior epdfview p7zip
