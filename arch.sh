eca2d55d49480f5cd50924150cff5899a26f96fc
kdnfgc <ren18124@gmail.com>
2021-02-07 14:14:56 -0700

    modified:   arch.sh

--- arch.sh
+++ arch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo pacman -S dunst dmenu kitty vim engrampa caja bspwm htop neofetch nitrogen polybar sxhkd redshift min scrot epdfview p7zip paru libreoffice-still gimp python2 python picom galculator viewnior
+sudo pacman -S dunst dmenu kitty vim engrampa caja bspwm htop neofetch nitrogen polybar sxhkd redshift min scrot epdfview p7zip paru libreoffice-still gimp python2 python picom galculator viewnior leafpad
 paru -S safeeyes
 paru -S doas
