015e91ed1606d7cb65016a868356a51ece717bda
kdnfgc <kdnfgc@protonmail.com>
2021-02-12 19:00:19 -0700

    modified:   arch.sh

--- arch.sh
+++ arch.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-sudo pacman -S dunst dmenu kitty vim engrampa caja bspwm htop neofetch nitrogen polybar sxhkd redshift min scrot epdfview p7zip paru libreoffice-still gimp python2 python picom galculator viewnior leafpad
-paru -S safeeyes
-paru -S doas
+sudo pacman -S dunst dmenu kitty vim engrampa caja bspwm htop neofetch nitrogen polybar sxhkd redshift min scrot epdfview p7zip libreoffice-still gimp python2 python galculator viewnior leafpad 
+yay -S safeeyes doas android-sdk android-sdk-build-tools android-sdk-platform-tools android-platform android-emulator
