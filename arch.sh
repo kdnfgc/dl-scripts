58dac80769c48149e57ab2d14dfa6581d7c32c95
kdnfgc <kdnfgc@protonmail.com>
2021-02-15 11:14:29 -0700

    modified:   arch.sh 	deleted:    fedora.sh 	deleted:    redhat.sh

--- arch.sh
+++ arch.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-sudo pacman -S dunst dmenu kitty vim engrampa caja bspwm htop neofetch nitrogen polybar sxhkd redshift min scrot epdfview p7zip libreoffice-still gimp python2 python galculator viewnior leafpad 
-yay -S safeeyes doas android-sdk android-sdk-build-tools android-sdk-platform-tools android-platform android-emulator
+sudo pacman -S dunst dmenu kitty vim engrampa caja bspwm htop neofetch nitrogen polybar sxhkd redshift min scrot epdfview p7zip libreoffice-still gimp python2 python galculator viewnior leafpad wmname
+sudo pacman -Syy && sudo pacman -S archlinuxcn-keyring
+sudo pacman -Syu android-studio
+yay -S safeeyes doas librewolf
--- fedora.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-<<<<<<< HEAD
-sudo dnf install dunst dmenu kitty vim engrampa caja bspwm htop leafpad neofetch nitrogen polybar sxhkd redshift firefox
-=======
-sudo dnf install dunst dmenu kitty vim engrampa caja bspwm htop leafpad neofetch nitrogen polybar sxhkd redshift min scrot eom
->>>>>>> ec49d163c42e2386317b64e01d63b5986f95bc38
