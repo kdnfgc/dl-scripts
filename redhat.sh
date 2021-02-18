17c572ad980500844f7b90c4d8c2ea065d245816
kdnfgc <kdnfgc@protonmail.com>
2021-02-18 10:12:14 -0700

    new file:   redhat.sh

---
+++ redhat.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo dnf install dunst dmenu kitty vim engrampa caja bspwm htop neofetch nitrogen polybar sxhkd redshift ungoogled-chromium scrot epdfview p7zip libreoffice-still gimp python2 python galculator viewnior leafpad wmname pa-applet picom
