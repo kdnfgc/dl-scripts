71d7b3ac79ab82188cb0baab7ab1117bbc56c31c
kdnfgc <kdnfgc@protonmail.com>
2021-02-27 13:38:45 -0700

    modified:   arch.sh 	modified:   arch2.sh

--- arch.sh
+++ arch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo pacman -S dunst dmenu kitty vim engrampa caja bspwm htop neofetch nitrogen polybar sxhkd redshift ungoogled-chromium scrot epdfview p7zip libreoffice-still gimp python2 python galculator viewnior leafpad wmname pa-applet picom
+sudo pacman -S dunst dmenu kitty vim engrampa caja bspwm htop neofetch nitrogen polybar sxhkd redshift ungoogled-chromium scrot epdfview p7zip libreoffice-still gimp python2 python galculator viewnior leafpad wmname pa-applet picom python-kivy wlroots waylandpp 
--- arch2.sh
+++ arch2.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-sudo pacman -Syy && sudo pacman -S archlinuxcn-keyring
-sudo pacman -Syu android-studio
 yay -S safeeyes doas 
