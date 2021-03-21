91358021bcdab03996adc2e8bed572bb11dff204
kdnfgc <kdnfgc@protonmail.com>
2021-03-20 18:52:10 -0600

    modified:   arch.sh

--- arch.sh
+++ arch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo pacman -S dunst dmenu kitty vim nvim engrampa caja bspwm htop neofetch nitrogen polybar sxhkd redshift ungoogled-chromium scrot epdfview p7zip libreoffice-still gimp python2 python galculator viewnior leafpad wmname pa-applet picom waylandpp nerd-fonts-noto-sans-mono nerd-fonts-roboto-mono intellij-idea-community-edition code inkscape rust ranger highlight libcaca atool mediainfo qt5ct termite menumaker kdeconnect
+sudo pacman -S dunst dmenu kitty vim nvim engrampa caja bspwm htop neofetch nitrogen polybar sxhkd redshift ungoogled-chromium scrot epdfview p7zip libreoffice-still gimp python2 python galculator viewnior leafpad wmname pa-applet picom waylandpp nerd-fonts-noto-sans-mono nerd-fonts-roboto-mono intellij-idea-community-edition code inkscape rust ranger highlight libcaca atool mediainfo qt5ct termite menumaker kdeconnect qemu libvirt edk2-ovmf virt-manager ebtables
