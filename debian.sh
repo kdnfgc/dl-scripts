9530c67a66f6afa9b62266f03723d0cdb8325242
kdnfgc <kdnfgc@protonmail.com>
2021-03-25 21:06:02 -0600

    modified:   arch.sh 	deleted:    arch2.sh 	deleted:    debian.sh 	deleted:    redhat.sh

--- debian.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo apt-get install gir1.2-appindicator3-0.1 gir1.2-notify-0.7 python3-psutil python3-xlib xprintidle python3-pip python3-babel python3-croniter dunst dmenu kitty vim nvim engrampa caja bspwm htop neofetch nitrogen polybar sxhkd redshift ungoogled-chromium scrot epdfview p7zip python2 python galculator viewnior leafpad wmname pa-applet compton nerd-fonts-noto-sans-mono nerd-fonts-roboto-mono intellij-idea-community-edition code inkscape rust ranger highlight libcaca atool mediainfo
-sudo pip3 install safeeyes
-sudo update-icon-caches /usr/share/icons/hicolor
--- redhat.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo dnf install dunst dmenu kitty vim engrampa caja bspwm htop neofetch nitrogen polybar sxhkd redshift ungoogled-chromium scrot epdfview p7zip libreoffice-still gimp python2 python galculator viewnior leafpad wmname pa-applet picom
