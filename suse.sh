1fb5aabe92534113ccbffe3148d84e005f6585df
kdnfgc <kdnfgc@protonmail.com>
2021-03-28 18:29:13 -0600

    new file:   suse.sh

---
+++ suse.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo zypper install neofetch safeeyes redshift redshift-gtk kitty dunst dmenu neovim caja engrampa caja-engrampa bspwm htop nitrogen polybar sxhkd scrot p7zip libreoffice gimp galculator viewnior leafpad wmname picom inkscape ranger qt5ct qemu libvirt ovmf-tools virt-manager dnsmasq bless
