c0f3fc0b1d767fff8dc5bd4e84bfde83efb8e8b9
kdnfgc <kdnfgc@protonmail.com>
2021-03-29 22:24:13 -0600

    modified:   solus.sh

--- solus.sh
+++ solus.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo eopkg install neofetch safeeyes redshift kitty adwaita-icon-theme plank vim neovim
+sudo eopkg install neofetch safeeyes redshift kitty adwaita-icon-theme plank vim neovim tmux
