0955245921f31e625aaed5f6f1801a5a4a53430a
kdnfgc <kdnfgc@protonmail.com>
2021-04-09 19:58:34 -0600

    modified:   solus.sh 	modified:   suse.sh

--- solus.sh
+++ solus.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-sudo eopkg install neofetch safeeyes redshift kitty adwaita-icon-theme plank vim neovim qt5ct vscode dash tmux zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo eopkg install neofetch safeeyes redshift kitty adwaita-icon-theme plank vim neovim qt5ct vscode dash zsh zsh-autosuggestions zsh-syntax-highlighting qemu libvirt ovmf virt-manager ebtables rustup 
+
+cargo install hunter
