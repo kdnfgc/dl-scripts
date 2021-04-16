bed882485d99d06e9f5a6c522f926ea278e07bd2
Kaden <kdn@localhost.localdomain>
2021-04-16 00:24:22 -0600

    modified:   suse.sh

--- suse.sh
+++ suse.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo zypper install neofetch safeeyes redshift redshift-gtk kitty neovim libreoffice gimp inkscape qt5ct qemu libvirt ovmf-tools virt-manager dnsmasq rust
+sudo zypper install neofetch safeeyes redshift redshift-gtk kitty neovim libreoffice gimp inkscape qt5ct qemu libvirt ovmf-tools virt-manager dnsmasq rust gcc 
