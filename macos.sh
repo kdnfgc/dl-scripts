cf77133a88c400c8c1bef836bc0a71f5b789baf2
Kaden <kdn@localhost.localdomain>
2021-04-18 17:50:58 -0600

    modified:   macos.sh 	modified:   suse.sh

---
+++ macos.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo brew install stow vim neovim cask
+sudo brew cask install geany kitty gimp inkscape libreoffice visual-studio-code
--- suse.sh
+++ suse.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-sudo zypper install neofetch safeeyes redshift redshift-gtk kitty neovim libreoffice gimp inkscape qt5ct qemu libvirt ovmf-tools virt-manager dnsmasq rust gcc make
+sudo zypper install safeeyes redshift redshift-gtk kitty neovim gimp inkscape qt5ct qemu libvirt ovmf-tools virt-manager dnsmasq rust gcc make curl
+
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave-browser
+sudo zypper addrepo https://packages.microsoft.com/yumrepos/vscode vscode
+sudo zypper refresh
+sudo zypper install brave-browser code
