882005c23aaa732f4c8884f71a59a99602906a5d
Kaden <kdn@localhost.localdomain>
2021-04-18 19:05:15 -0600

    modified:   macos.sh 	modified:   suse.sh

--- macos.sh
+++ macos.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+#Installs Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+#Adds Vim-Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#Installs Most Utils
 sudo brew install stow vim neovim cask
 sudo brew cask install geany kitty gimp inkscape libreoffice visual-studio-code
+
+#Adds My Dotfiles
+git clone https://github.com/kdnfgc/.dotfiles.git
+cd .dotfiles
+stow geany kitty vim nvim p10kux zsh
+
+#Adds Nerd Fonts
+cd root/root/usr/share/fonts/truetype
+sudo cp -r robotomono ~/Library/Fonts
--- suse.sh
+++ suse.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-sudo zypper install safeeyes redshift redshift-gtk kitty neovim gimp inkscape qt5ct qemu libvirt ovmf-tools virt-manager dnsmasq rust gcc make curl
+#Adds Vim-Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+#Installs most utilities
+sudo zypper install stow zsh dash safeeyes redshift redshift-gtk kitty neovim gimp inkscape qt5ct qemu libvirt ovmf-tools virt-manager dnsmasq rust gcc make curl
+
+#Adds and installs Brave and VSCode
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave-browser
 sudo zypper addrepo https://packages.microsoft.com/yumrepos/vscode vscode
 sudo zypper refresh
 sudo zypper install brave-browser code
+
+#Adds my dotfiles
+git clone https://github.com/kdnfgc/.dotfiles.git
+cd .dotfiles
+stow geany kitty vim nvim p10klx pic plank qt5ct zsh
+
+#Adds Nerd Fonts
+cd root/root/usr/share/fonts
+sudo cp -r truetype /usr/share/fonts
