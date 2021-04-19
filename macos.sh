0bd6199b207a47fe99e62aa172042c50c3a13cee
Kaden <kdn@localhost.localdomain>
2021-04-18 18:04:41 -0600

    modified:   macos.sh

--- macos.sh
+++ macos.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 sudo brew install stow vim neovim cask
 sudo brew cask install geany kitty gimp inkscape libreoffice visual-studio-code
