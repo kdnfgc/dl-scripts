bcfec8743033c38fd0a9b1fc9c2c6564d79e7286
Kaden <kdn@localhost.localdomain>
2021-04-23 12:20:09 -0600

    modified:   suse.sh

--- suse.sh
+++ suse.sh
@@ -19,8 +19,10 @@ sudo zypper in brave-browser codium
 git clone https://github.com/sainnhe/gruvbox-material-gtk.git
 #If no .icons folder, add icons folder (pseudocode)
 mv ~/gruvbox-material-gtk/icons/* ~/.icons
+sudo cp -r ~/.icons/* /usr/share/icons
 #If no .themes folder, add themes folder (pseudocode)
 mv ~/gruvbox-material-gtk/themes/* ~/.themes
+sudo cp -r ~/.themes /usr/share/themes
 
 #Remove leftover files
 rm -r gruvbox-material-gtk
@@ -29,6 +31,9 @@ rm -r gruvbox-material-gtk
 git clone https://github.com/kdnfgc/.dotfiles.git
 cd .dotfiles
 stow geany kitty vim nvim p10klx pic plank qt5ct zsh profile
+
+#Removes unnecessary branding
+sudo zypper rm yast2-qt-branding-openSUSE
 
 #Adds Nerd Fonts
 cd root/root/usr/share/fonts
