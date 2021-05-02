b1f5f5220251947106d7e7ee9a0e5a4cfb07b74d
kdnfgc <kdnfgc@protonmail.com>
2021-05-02 12:52:46 -0600

    modified:   fedora.sh 	modified:   suse.sh

--- fedora.sh
+++ fedora.sh
@@ -1,3 +1,33 @@
 #!/bin/sh
 
+#Adds Nerd Fonts
+cd root/root/usr/share/fonts
+sudo cp -r truetype /usr/share/fonts
+cd
 
+#Adds halmak layout
+cd .dotfiles/root/root/usr/share/X11/xkb
+sudo cp -r rules /usr/share/X11/xkb
+sudo cp -r symbols /usr/share/X11/xkb
+sudo cp -r types /usr/share/X11/xkb
+cd
+
+
+#Adds Vim-Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#Installs pfetch
+git clone https://github.com/dylanaraps/pfetch.git
+cd pfetch
+make
+sudo make install
+cd
+
+#Adds Gruvbox-Material theme
+git clone https://github.com/sainnhe/gruvbox-material-gtk.git
+cd gruvbox-material-gtk
+sudo cp -r icons/* /usr/share/icons
+sudo cp -r themes/* /usr/share/themes
+cd
+sudo rm -r gruvbox-material-gtk
--- suse.sh
+++ suse.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-#Adds Vim-Plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #Installs most utilities
 sudo zypper in stow zsh dash safeeyes redshift redshift-gtk kitty neovim gimp inkscape qt5ct rust gcc make curl hte 
@@ -13,32 +10,53 @@ sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave
 sudo zypper refresh
 sudo zypper in brave-browser 
 
-#Adds Icons/Themes
-git clone https://github.com/sainnhe/gruvbox-material-gtk.git
-#If no .icons folder, add icons folder (pseudocode)
-mv ~/gruvbox-material-gtk/icons/* ~/.icons
-sudo cp -r ~/.icons/* /usr/share/icons
-#If no .themes folder, add themes folder (pseudocode)
-mv ~/gruvbox-material-gtk/themes/* ~/.themes
-sudo cp -r ~/.themes /usr/share/themes
+#Removes default profile
+sudo rm .profile
 
-#Remove leftover files
-rm -r gruvbox-material-gtk
+#Adds my dotfiles
+git clone https://github.com/kdnfgc/.dotfiles.git
+cd .dotfiles
+stow kitty vim nvim p10kux pic plank qt5ct zsh profile
+cd
+
+#Adds Nerd Fonts
+cd root/root/usr/share/fonts
+sudo cp -r truetype /usr/share/fonts
+cd
+
+#Adds halmak layout
+cd .dotfiles/root/root/usr/share/X11/xkb
+sudo cp -r rules /usr/share/X11/xkb
+sudo cp -r symbols /usr/share/X11/xkb
+sudo cp -r types /usr/share/X11/xkb
+cd
 
 #Adds Vim-Plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-#Adds my dotfiles
-git clone https://github.com/kdnfgc/.dotfiles.git
-cd .dotfiles
-stow kitty vim nvim p10klx pic plank qt5ct zsh profile
+#Make zsh my default theme
+chsh -s /bin/zsh
+
+#Installs pfetch
+git clone https://github.com/dylanaraps/pfetch.git
+cd pfetch
+make
+sudo make install
+cd
+
+#Adds Gruvbox-Material theme
+git clone https://github.com/sainnhe/gruvbox-material-gtk.git
+cd gruvbox-material-gtk
+sudo cp -r icons/* /usr/share/icons
+sudo cp -r themes/* /usr/share/themes
+cd
+sudo rm -r gruvbox-material-gtk
+
 
 #Removes unnecessary branding
 sudo zypper rm yast2-qt-branding-openSUSE
 sudo zypper al yast2-qt-branding-openSUSE
 
-#Adds Nerd Fonts
-cd root/root/usr/share/fonts
-sudo cp -r truetype /usr/share/fonts
+
 
