db88cf9be9681894b05d448c80dbb32c9279a3d8
kdnfgc <kdnfgc@protonmail.com>
2021-06-10 11:14:10 -0600

    modified:   suse.sh

--- suse.sh
+++ suse.sh
@@ -4,12 +4,10 @@
 cd
 
 #Installs most utilities
-sudo zypper in stow zsh dash safeeyes neovim gimp inkscape rust gcc make curl gnome-photos evolution evolution-plugin-rss deja-dup gnome-characters gnome-screenshot gnome-session-wayland gnome-builder glade docker yast2-docker zypper-docker docker-machine-drivers-kvm2 gnome-calendar gnome-contacts 
+sudo zypper in stow zsh dash safeeyes neovim gimp inkscape rust gcc make curl gnome-photos deja-dup gnome-characters gnome-screenshot gnome-session-wayland gnome-builder glade docker yast2-docker zypper-docker docker-machine-drivers-kvm2 gnome-calendar gnome-contacts gnome-dictionary gnome-weather -y 
 
-#Adds flatpak repo
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-#Adds and installs Brave
+#Adds and installs Brave and VSCode
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave-browser
@@ -35,11 +33,11 @@ sudo cp -r truetype /usr/share/fonts
 cd
 
 #Adds halmak layout
-cd .dotfiles/root/root/usr/share/X11/xkb
+cd ~/.dotfiles/root/root/usr/share/X11/xkb
 sudo cp -r rules /usr/share/X11/xkb
 sudo cp -r symbols /usr/share/X11/xkb
 sudo cp -r types /usr/share/X11/xkb
-cd .dotfiles/root/root/etc/X11/xorg.conf.d
+cd ~/.dotfiles/root/root/etc/X11/xorg.conf.d
 sudo cp 00-keyboard.conf /etc/X11/xorg.conf.d
 cd
 
@@ -58,12 +56,12 @@ sudo make install
 cd
 
 #Adds Gruvbox-Material theme
-git clone https://github.com/sainnhe/gruvbox-material-gtk.git
-cd gruvbox-material-gtk
-sudo cp -r icons/* /usr/share/icons
-sudo cp -r themes/* /usr/share/themes
-cd
-sudo rm -r gruvbox-material-gtk
+#git clone https://github.com/sainnhe/gruvbox-material-gtk.git
+#cd gruvbox-material-gtk
+#sudo cp -r icons/* /usr/share/icons
+#sudo cp -r themes/* /usr/share/themes
+#cd
+#sudo rm -r gruvbox-material-gtk
 
 
 #Removes unnecessary branding
